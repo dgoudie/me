9b2693dfa84f60a0bcbd34d903cb4accd495d58b
dgoudie <34222208+dgoudie@users.noreply.github.com>
2019-07-16 20:26:12 -0500

    updated to scss, added some initial stuff

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './Home.scss';
 
 class Home extends Component {
     public render() {
         return (
-            <div>
-                Home
+            <div className="home">
+                <header>
+                    <div className="header-inner">
+                        <h2>Daniel Goudie</h2>
+                        <h3>Full Stack Software Developer</h3>
+                        <h1>Hello!</h1>
+                    </div>
+                </header>
             </div>
         );
     }
