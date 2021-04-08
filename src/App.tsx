a3f66d4b6c06ab36fc1c0e3b8ad4e7f98b18b147
dgoudie <34222208+dgoudie@users.noreply.github.com>
2021-04-08 10:30:33 -0500

    added new types, removed extra deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
-import 'react-photoswipe/lib/photoswipe.css';
-
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import Home from 'views/home/Home';
-import { PhotoSwipe } from 'react-photoswipe';
 import React from 'react';
-
-export type PhotoSwipeContextType = {
-  items: PhotoSwipe.Item[] | null;
-  onClose: () => void;
-};
-
-export const PhotoSwipeContext = React.createContext<PhotoSwipeContextType>({
-  items: null,
-  onClose: () => null,
-});
 
 export interface HomePageParams {
   stackItemId?: string;
 }
 
 function App() {
-  const context: PhotoSwipeContextType = {
-    items: null,
-    onClose: () => null,
-  };
   return (
-    <PhotoSwipeContext.Provider value={context}>
-      <Router>
-        <Route path="/:stackItemId?" exact component={Home} />
-      </Router>
-      <PhotoSwipeContext.Consumer>
-        {({ items, onClose }) => (
-          <PhotoSwipe
-            items={items ?? []}
-            onClose={onClose}
-            isOpen={!!items?.length}
-          />
-        )}
-      </PhotoSwipeContext.Consumer>
-    </PhotoSwipeContext.Provider>
+    <Router>
+      <Route path="/:stackItemId?" exact component={Home} />
+    </Router>
   );
 }
 
