5fef0be9ed27d8ce1057b408e68fd88cde483927
dgoudie <34222208+dgoudie@users.noreply.github.com>
2025-11-22 14:05:25 -0600

    add images to project

--- src/lib/assets/data/education.ts
+++ src/lib/assets/data/education.ts
@@ -1,6 +1,6 @@
 export const education = [
 	{
-		iconUrl: 'https://cdn.goudie.dev/images/me/uno.png',
+		iconUrl: '/uno.png',
 		title: "Bachelor's Degree in Cyber-Security",
 		secondaryInfo: ['Minor in Computer Science', 'University of Nebraska at Omaha', '2014 - 2018']
 	}
--- src/lib/assets/data/work-experience.ts
+++ src/lib/assets/data/work-experience.ts
@@ -1,6 +1,6 @@
 export const workExperience = [
 	{
-		iconUrl: 'https://cdn.goudie.dev/images/up.svg',
+		iconUrl: '/up.svg',
 		company: 'Union Pacific Railroad',
 		jobTitle: 'Associate Systems Engineer, Platforms Engineering',
 		startDate: '2022-05-01T06:00:00.000Z',
@@ -9,7 +9,7 @@ export const workExperience = [
 			'I am currently working on and am one of the lead front- and back-end engineers for Union Pacific\'s internally developed state-of-the-art observability platform called "Unified Systems Management". Additionally, I am a member of UP\'s enterprise API approval board. Our goal is to ensure consistency among our API suite, enforce enterprise standards, and provide feedback for APIs being developed throughout the company.'
 	},
 	{
-		iconUrl: 'https://cdn.goudie.dev/images/up.svg',
+		iconUrl: '/up.svg',
 		company: 'Union Pacific Railroad',
 		jobTitle: 'Senior Project Engineer, Corporate Technology',
 		startDate: '2020-04-01T05:00:00.000Z',
@@ -18,7 +18,7 @@ export const workExperience = [
 			"At my previous position on the Corporate Technology developer team, I was the lead technical designer for the company's all-encompassing real estate management software - I was responsible for selecting the tech stack for the application and was the lead UI developer. I also assisted in debugging and designing of other real estate and financial applications."
 	},
 	{
-		iconUrl: 'https://cdn.goudie.dev/images/up.svg',
+		iconUrl: '/up.svg',
 		company: 'Union Pacific Railroad',
 		jobTitle: 'Application Developer',
 		startDate: '2018-05-07T05:00:00.000Z',
