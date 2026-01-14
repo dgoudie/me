e7f1f8019243b220aedfadfa5c2c8ee6094fd728
dgoudie <34222208+dgoudie@users.noreply.github.com>
2026-01-13 19:32:19 -0600

    update website description to clarify JavaScript framework usage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,8 +25,10 @@
 				<Hobbies />
 			</div>
 			<div class="source">
-				I built this website using <a href="https://svelte.dev/" target="_blank">Svelte</a>. View
-				the source
+				I built this website usingthe JavaScript framework <a
+					href="https://svelte.dev/"
+					target="_blank">Svelte</a
+				>. View the source
 				<a href="https://github.com/dgoudie/me" target="_blank">here</a>.
 			</div>
 			<div class="website">
